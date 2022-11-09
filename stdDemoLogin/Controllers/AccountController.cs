824f890035fe0ca336ece18ac831350f6609f7e2
Koushik Halder <koushikhalder22798@hotmail.com>
2022-11-09 15:31:43 +0530

    Add project files.

---
+++ stdDemoLogin/Controllers/AccountController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace stdDemoLogin.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult stdLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult stdLogin(studentLogin stdLogin)
+        {
+            using (var context=new StudentDBEntities())
+            {
+                bool isValidStudent = context.studentLogin.Any(x => x.rollNumber == stdLogin.rollNumber && x.password == stdLogin.password);
+                if (isValidStudent)
+                {
+                    FormsAuthentication.SetAuthCookie(stdLogin.rollNumber.ToString(),false);
+                    return RedirectToAction("Index", "studentDetails");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "invalid credentials");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult signUp()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult signUp(studentLogin student)
+        {
+            using (var context = new StudentDBEntities())
+            {
+                context.studentLogin.Add(student);
+                context.SaveChanges();
+            }
+            return RedirectToAction("stdLogin");
+        }
+
+
+        public ActionResult logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("stdLogin");
+        }
+    }
+}
---
+++ stdDemoLogin/Controllers/studentDetailsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using stdDemoLogin;
+
+namespace stdDemoLogin.Controllers
+{
+    [Authorize]
+    public class studentDetailsController : Controller
+    {
+        private StudentDBEntities db = new StudentDBEntities();
+
+        // GET: studentDetails
+        public ActionResult Index()
+        {
+            return View(db.studentDetails.ToList());
+        }
+
+        // GET: studentDetails/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            studentDetails studentDetails = db.studentDetails.Find(id);
+            if (studentDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetails);
+        }
+
+        // GET: studentDetails/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: studentDetails/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "rollNumber,name,address,pincode")] studentDetails studentDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                db.studentDetails.Add(studentDetails);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(studentDetails);
+        }
+
+        // GET: studentDetails/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            studentDetails studentDetails = db.studentDetails.Find(id);
+            if (studentDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetails);
+        }
+
+        // POST: studentDetails/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "rollNumber,name,address,pincode")] studentDetails studentDetails)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentDetails).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(studentDetails);
+        }
+
+        // GET: studentDetails/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            studentDetails studentDetails = db.studentDetails.Find(id);
+            if (studentDetails == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentDetails);
+        }
+
+        // POST: studentDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            studentDetails studentDetails = db.studentDetails.Find(id);
+            db.studentDetails.Remove(studentDetails);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
